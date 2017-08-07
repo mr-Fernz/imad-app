108143126f33dcdb94a6c29223ae562a519796d4
mr-Fernz <anzilfernandes4@gmail.com>
2017-08-08 01:08:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ function createTemplate (data) {
     var date = data.date;
     var content = data.content;
     
-    var htmltemplate = `
+    var htmlTemplate = `
     <html>
         <head>
             <title>
