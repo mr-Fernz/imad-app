5e9a4a422f558976ba8c5bd2cbd07c157c652e61
mr-Fernz <anzilfernandes4@gmail.com>
2017-08-10 23:25:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,12 @@ var img = document.getElementById('madi');
 img.onclick = function()
 {
 
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
 };
 
 var marginLeft = 0;
 function moveRight()
 {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
