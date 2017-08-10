5718c87c805a7805ff272693a269de45a0494c0f
mr-Fernz <anzilfernandes4@gmail.com>
2017-08-10 23:31:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,17 @@ img.onclick = function()
     var interval = setInterval(moveRight,50);
 };
 
+var div = document.getElementById('div');
+div.onclick = function()
+{
+    var int = setInterval(moveUp,50);
+};
+function moveUp()
+{
+    marginUp = marginUp + 5;
+    div.style.marginUp = marginUp + 'px';
+}
+
 var marginLeft = 0;
 function moveRight()
 {
