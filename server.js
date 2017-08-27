3283e1e99113443e91badb1499f012c776095888
mr-Fernz <anzilfernandes4@gmail.com>
2017-08-28 01:09:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
-var bodyParser = require('bodyParser');
+var bodyParser = require('body-parser');
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
@@ -90,10 +90,10 @@ app.get('/hash/:input', function(req,res) {
 });
 
 app.post('/create_user', function(req,res) {
-   var salt = RandomBytes(128).tostring('hex');
-   var dbstring = hash(password, salt);
    var username = req.body.username;
    var password = req.body.password;
+   var salt = RandomBytes(128).tostring('hex');
+   var dbstring = hash(password, salt);
    pool.query('INSERT INTO "user"(username, password) VAUES ($1 , $2)', [username,dbstirng], function(err, result) {
        if (err)
             res.status(500).send(err, tostring());
