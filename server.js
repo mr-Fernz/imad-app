bfb1176b8ed16df660d54582ff40790b3a2849e8
mr-Fernz <anzilfernandes4@gmail.com>
2017-08-28 01:17:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/hash/:input', function(req,res) {
     res.send(hashedString);
 });
 
-app.post('/create_user', function(req,res) {
+app.post('/create-user', function(req,res) {
    var username = req.body.username;
    var password = req.body.password;
    var salt = RandomBytes(128).tostring('hex');
