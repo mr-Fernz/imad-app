6e3841f1afff5171bb975eb67c4b6526455cafc9
mr-Fernz <anzilfernandes4@gmail.com>
2017-08-28 01:47:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.post('/create-user', function(req,res) {
    var username = req.body.username;
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
-   var dbstring = hash(password, salt);
+   var dbString = hash(password, salt);
    pool.query('INSERT INTO "user"(username, password) VAUES ($1 , $2)', [username,dbstirng], function(err, result) {
        if (err)
             res.status(500).send(err, tostring());
