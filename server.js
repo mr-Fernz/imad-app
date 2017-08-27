7d31cf5e8c02475aa3bc3a7eef051f8a2d0331d8
mr-Fernz <anzilfernandes4@gmail.com>
2017-08-28 01:59:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var config =  {
     user: 'anzilfernandes4',
     database: 'anzilfernandes4',
-    host: 'http://db.imad.hasura-app.io',
+    host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
 };
